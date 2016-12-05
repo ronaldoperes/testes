2eba4946a04e6f3185639bc2e6af89040f130683
Peres <ronaldoperes@gmail.com>
2016-12-05 19:52:12 -0200

    temperatures2

--- Testes/ConsoleApplication1/Program.cs
+++ Testes/ConsoleApplication1/Program.cs
@@ -10,18 +10,35 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            // exercicio TEMPERATURES do codingame
             int n = 5;// int.Parse(Console.ReadLine()); // the number of temperatures to analyse
             string temps = "42 -5 -2 3 4 55 12 21 -5 24";// Console.ReadLine(); // the n temperatures expressed as integers ranging from -273 to 5526
-
+            string sdc= "42 -5 -2 3 4 55 12 21 -5 24";
             Console.Error.WriteLine(temps);
             Console.Error.WriteLine(n);
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
+            int minimum = sdc
+                           .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .OrderBy(Math.Abs)
+                           .ThenByDescending(x => x)
+                           .FirstOrDefault();
 
+            var minimum2 = sdc
+                          .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .OrderBy(Math.Abs);
+
+            var min3 = minimum2.ThenByDescending(x => x);
+                          
+
+            Console.WriteLine(minimum2.FirstOrDefault());
 
             string[] str;
 
-            if (temps.Length > 0)
+            if (!string.IsNullOrEmpty(temps))
 
             {
                 str = temps.Split(' ');
