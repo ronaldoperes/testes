2b0bfde5e739054fab147676979d5c1e609f954d
Peres <ronaldoperes@gmail.com>
2016-12-05 17:08:59 -0200

    teste2

--- Testes/ConsoleApplication1/Program.cs
+++ Testes/ConsoleApplication1/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1);
+            int width = int.Parse(Console.ReadLine()); // the number of cells on the X axis
+            int height = int.Parse(Console.ReadLine()); // the number of cells on the Y axis
+
+            string[,] matrix = new string[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                string line = Console.ReadLine(); // width characters, each either 0 or .
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = line[j].ToString();
+                }
+
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+            // Three coordinates: a node, its right neighbor, its bottom neighbor
+            //Console.WriteLine("0 0 1 0 0 1");
         }
     }
 }
